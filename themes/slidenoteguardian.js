8c5d6fc870695a239dd7d82e86e1531c270b603c
jonny <dekan@gaenseklein.net>
2018-07-07 00:52:28 +0200

    added imagessupport

--- themes/slidenoteguardian.js
+++ themes/slidenoteguardian.js
@@ -43,10 +43,19 @@ slidenoteGuardian.prototype.loadNote = async function(destination){
     this.encBufferString = this.encBufferString.substring(this.ivlength);//delete iv-chars from string
     let buffer = new Uint8Array(this.encBufferString.length);
     for(let i=0;i<this.encBufferString.length;i++)buffer[i]=this.encBufferString.charCodeAt(i)-255;
-    this.encTextBuffer = buffer.buffer; //changing to ArrayBuffer
-    this.decText = await this.decrypt(buffer.buffer); //decrypt ArrayBuffer
+    //this.encTextBuffer = buffer.buffer; //changing to ArrayBuffer -- TODO:kann weg oder?
+    this.decText = await this.decrypt(buffer.buffer, this.iv); //decrypt ArrayBuffer
     this.slidenote.textarea.value = this.decText; //putting result into textarea
+    //loading images:
+    let imgstring;
+    if(destination==="cms")imgstring = this.cmsImages.value;
+    if(destination==="local")imgstring = this.localstorage.getItem('cryptimagestring');
+    if(imgstring != undefined && imgstring.length>0){
+      //images sind vorhanden - TODO: check ob images bereits geladen sind mittels timestamp
+      this.encImageString = imgstring;
+      await this.loadImages(destination);
 
+    }
     //cleaning up:
     this.slidenote.textarea.focus(); //focus on textarea for further editing
     this.slidenote.parseneu(); //let wysiwyg-editor notice change
@@ -56,7 +65,9 @@ slidenoteGuardian.prototype.loadNote = async function(destination){
 slidenoteGuardian.prototype.saveNote = async function(destination){
   //saves Note to cmsArea -> CMS or to local destination
   //destination is cms or local - will be encrypted nevertheless
-  let encTextBuffer = await this.encrypt(slidenote.textarea.value);
+  let encResult = await this.encrypt(slidenote.textarea.value);
+  let encTextBuffer = encResult.encbuffer;
+  let iv = encResult.iv;
   //getting only displayable chars without control-chars:
   let utf8array = new Uint8Array(encTextBuffer); //changing into utf8-Array
   //console.log(utf8array);
@@ -66,8 +77,13 @@ slidenoteGuardian.prototype.saveNote = async function(destination){
   }
   //converting iv to string with same method:
   let ivstring="";
-  for(let i=0; i<this.iv.length;i++)ivstring+=String.fromCharCode(this.iv[i]+255);
+  for(let i=0; i<iv.length;i++)ivstring+=String.fromCharCode(iv[i]+255);
   let result = ivstring+utf8string;//save iv in front of code
+  //save Images:
+  if(this.slidenote.base64images.notempty){
+    //Images sind vorhanden - TODO: hier später weiterer check ob bereits gesichert ist
+    await this.saveImages(destination);
+  }
   if(destination ==="cms"){
     this.cmsArea.value= result; //putting it in textarea of cms
     //if(imagestring.length>0)this.cmsImages.value = ivstring+imagestring;
@@ -84,16 +100,17 @@ slidenoteGuardian.prototype.saveNote = async function(destination){
 
 slidenoteGuardian.prototype.loadImages = async function(destination){
   //image-part:
-  if(this.encImageString.length>0){
+  let encimagestring = this.encImageString.substring(0);
+  //console.log("load Images" + encimagestring.substring(this.ivlength,30));
+  if(encimagestring.length>0){
     //getting iv out of the string
     let imgiv = new Uint8Array(this.ivlength);
-    for(let iiv=0;iiv<this.ivlength;iiv++)imgiv[i]=this.encImageString.charCodeAt(iiv)-255;
-    this.imgiv = imgiv;
-    this.encImageString = this.encImageString.substring(this.ivlength); //delete iv-chars
-    let imgbuffer = new Uint8Array(this.encImageString.length);
-    for(let im=0;im<imgbuffer.length;im++)this.encImageString.charCodeAt(im)-255;
-    //this.encImageBuffer = imgbuffer.buffer;
-    let decImageString = await this.decrypt(imgbuffer.buffer); //decrypt imgbuffer
+    for(let iiv=0;iiv<this.ivlength;iiv++)imgiv[iiv]=encimagestring.charCodeAt(iiv)-255;
+    //this.imgiv = imgiv;
+    encimagestring = encimagestring.substring(this.ivlength); //delete iv-chars
+    let imgbuffer = new Uint8Array(encimagestring.length);
+    for(let im=0;im<imgbuffer.length;im++)imgbuffer[im]=encimagestring.charCodeAt(im)-255;
+    let decImageString = await this.decrypt(imgbuffer.buffer, imgiv); //decrypt imgbuffer
     this.slidenote.base64images.loadImageString(decImageString); //send it to slidenote
 
   }
@@ -102,10 +119,19 @@ slidenoteGuardian.prototype.loadImages = async function(destination){
 slidenoteGuardian.prototype.saveImages = async function(destination){
   //now the images:
   let imagestring="";
+  console.log("save Imagestring:"+this.slidenote.base64images.allImagesAsString());
   if(this.slidenote.base64images.base64images.length>0){ //muss diese abfrage überhaupt hier sein?
-    let imageBuffer = await this.encrypt(this.slidenote.base64images.allImagesAsString());
+    let encResult = await this.encrypt(this.slidenote.base64images.allImagesAsString());
+    let imageBuffer = encResult.encbuffer;
+    for(let i=0;i<encResult.iv.length;i++)imagestring+=String.fromCharCode(encResult.iv[i]+255);
     let imageutf8 = new Uint8Array(imageBuffer);
     for(let i=0;i<imageutf8.length;i++)imagestring+=String.fromCharCode(imageutf8[i]+255);
+    if(destination==="cms"){
+      this.cmsImages.value=imagestring;
+    }else if(destination==="local"){
+      console.log("cryptimagestring:"+imagestring);
+      this.localstorage.setItem('cryptimagestring',imagestring);
+    }
   }
 };
 
@@ -121,21 +147,26 @@ slidenoteGuardian.prototype.saveConfig = async function(destination){
 
 }
 slidenoteGuardian.prototype.encrypt = async function(plaintext){
+  console.log("encrypt plaintext:"+plaintext);
     let plainTextUtf8 = new TextEncoder().encode(plaintext); //changing into UTF-8-Array
-    await this.createKey();
-    return await crypto.subtle.encrypt(this.alg, this.key, plainTextUtf8);
+    let keyguardian = await this.createKey();
+    //this.iv = keyguardian.iv;
+    let encbuffer = await crypto.subtle.encrypt(keyguardian.alg, keyguardian.key, plainTextUtf8);
+    return {encbuffer: encbuffer, iv:keyguardian.iv};
     /*the job of encrypt is done - rest of code should be in save*/
 }
-slidenoteGuardian.prototype.decrypt = async function(buffer){
-  await this.createKey(this.iv);
+slidenoteGuardian.prototype.decrypt = async function(buffer, iv){
+  let keyguardian = await this.createKey(iv);
   console.log("decoding starts");
   try{
-    this.plainTextBuffer = await this.crypto.subtle.decrypt(this.alg, this.key, buffer);
+    this.plainTextBuffer = await this.crypto.subtle.decrypt(keyguardian.alg, keyguardian.key, buffer);
   } catch(e){
     console.log(e);
+    console.log("decryption has failed!");
+    return "decryption has failed";
   }
   console.log("decoding has ended");
-  return new TextDecoder().decode(this.plainTextBuffer);
+  return new TextDecoder().decode(this.plainTextBuffer); //TODO: error-handling
 }
 
 //helper functions - for internal use only:
@@ -145,13 +176,14 @@ slidenoteGuardian.prototype.createKey = async function(iv){
   if(this.password == null)this.password = prompt("please type in your personal password");
   let pwUtf8 = new TextEncoder().encode(this.password);
   let passwordHash = await this.crypto.subtle.digest('SHA-256', pwUtf8);
-
+  let keyguardian = {};
   if(iv==null){
-    this.iv = crypto.getRandomValues(new Uint8Array(this.ivlength));
+    keyguardian.iv = crypto.getRandomValues(new Uint8Array(this.ivlength));
   }else{
-    this.iv = iv;
+    keyguardian.iv = iv;
   }
-  this.alg = { name: 'AES-GCM', iv: this.iv };
-  this.key = await crypto.subtle.importKey('raw', passwordHash, this.alg, false, ['encrypt', 'decrypt']);
+  keyguardian.alg = { name: 'AES-GCM', iv: keyguardian.iv };
+  keyguardian.key = await crypto.subtle.importKey('raw', passwordHash, keyguardian.alg, false, ['encrypt', 'decrypt']);
   console.log("key created");
+  return keyguardian;
 }
