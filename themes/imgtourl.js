1a9914caf23b6bcfd28a107aeb79701af78b69ab
jonny <dekan@gaenseklein.net>
2018-08-31 15:51:19 +0200

    first try resizing images

--- themes/imgtourl.js
+++ themes/imgtourl.js
@@ -13,6 +13,10 @@ function base64Image(name,base64url){
 slidenote.base64images = {
   base64images : new Array(),
   preselectedname: null,
+  quality: 0.8,
+  imagetype: "image/jpeg",
+  maxwidth: 1280, //1920,
+  maxheight: 720, //1080,
   imageByName : function(imagename){  //returns fitting base64-image
     for(var x=0;x<this.base64images.length;x++){
         if(this.base64images[x].name===imagename){
@@ -112,7 +116,7 @@ newtheme.styleThemeSpecials = function(){
       imgtags[x].src=b64image.base64url;
     }
   }
-}
+};
 newtheme.init = function(){
   console.log("imgtourl wird initialisiert");
   //add imagebutton to texteditorbuttons
@@ -127,7 +131,7 @@ newtheme.init = function(){
     var el = slidenote.parser.CarretOnElement(slidenote.textarea.selectionEnd);
     if(el!=null && el.typ==="image"){
       slidenote.base64images.preselectedname = el.src;
-      var b64image = slidenote.base64images.imageByName(el.src)
+      var b64image = slidenote.base64images.imageByName(el.src);
       if(b64image !=null){
         var fprew = document.getElementById("filePreview");
         fprew.innerHTML="";
@@ -142,7 +146,7 @@ newtheme.init = function(){
     }
 
     document.getElementById('imagesblock').classList.add('visible');
-  }
+  };
   texteditorbuttons.appendChild(button);
 
   console.log(fileInput);
@@ -159,10 +163,60 @@ newtheme.init = function(){
   					fileDisplayArea.innerHTML = "";
 
   					var img = new Image();
-  					img.src = reader.result;
+            img.onload = function() {
+              var maxwidth = slidenote.base64images.maxwidth;
+              var width = maxwidth;
+              var maxheight = slidenote.base64images.maxheight;
+              if(img.width<maxwidth){
+                console.log("image has only width:"+img.width);
+                document.getElementById("downsizedimage").src = img.src;
+                return;
+              }
+               var canvas = document.createElement('canvas'),
+                   ctx = canvas.getContext("2d"),
+                   oc = document.createElement('canvas'),
+                   octx = oc.getContext('2d');
 
-            img.onclick = function(){slidenote.base64images.addImage(nombre,reader.result);};
-  					fileDisplayArea.appendChild(img);
+               canvas.width = maxwidth; // destination canvas size
+               canvas.height = canvas.width * img.height / img.width;
+
+               if(canvas.height > maxheight){
+                 canvas.height = maxheight;
+                 canvas.width = canvas.height * img.width / img.height;
+                 width = canvas.width;
+               }
+
+               var cur = {
+                 width: Math.floor(img.width * 0.5),
+                 height: Math.floor(img.height * 0.5)
+               }
+
+               oc.width = cur.width;
+               oc.height = cur.height;
+
+               octx.drawImage(img, 0, 0, cur.width, cur.height);
+
+               while (cur.width * 0.5 > width) {
+                 cur = {
+                   width: Math.floor(cur.width * 0.5),
+                   height: Math.floor(cur.height * 0.5)
+                 };
+                 octx.drawImage(oc, 0, 0, cur.width * 2, cur.height * 2, 0, 0, cur.width, cur.height);
+               }
+
+               ctx.drawImage(oc, 0, 0, cur.width, cur.height, 0, 0, canvas.width, canvas.height);
+               //document.getElementById("downsizedimage").src = canvas.toDataURL();
+               document.getElementById("downsizedimage").src = canvas.toDataURL(slidenote.base64images.imagetype,slidenote.base64images.quality);
+            }
+            var targetimg = new Image();
+            targetimg.id = "downsizedimage";
+            targetimg.name = nombre;
+            targetimg.onclick = function(){slidenote.base64images.addImage(this.name,this.src);};
+
+  					fileDisplayArea.appendChild(targetimg);
+            //load image
+            img.src = reader.result;
+
             //add to slidenote:
             //slidenote.base64images.addImage(nombre,reader.result);
 
