5b752c7e3bd444782b4d0cce096f2f3ff1ea9c67
jonny <dekan@gaenseklein.net>
2018-09-14 16:16:55 +0200

    fixed procontra to work now with "list

--- themes/procontra.js
+++ themes/procontra.js
@@ -8,10 +8,10 @@ themeprocontra.description = "Create Pro-Contra-Style Blocks";
 /* anschließend wird dem theme ein spezieller style zugewiesen:
 * er besteht aus einem suchmuster (array mit zeilencodes) sowie einem start und end-html-tag
 */
-themeprocontra.addStyle(new Array("h2","ol","h2","ol"),'<div class="procontra">',"</div>");
+themeprocontra.addStyle(new Array("h2","list"),'<div class="listblock">',"</div>");
 
-//wir hängen zum test noch einen weiteren style an, nämlich ul
-themeprocontra.addStyle(new Array("h2","ul","h2","ul"),'<div class="procontra">',"</div>");
+//jetzt ummanteln wir mit dem stil pro-contra zwei listblöcke:
+themeprocontra.addStyle(new Array("h2","list","h2","list"),'<div class="procontra">',"</div>");
 
 /* unser beispiel ist hiermit schon zu ende
 * es fehlt nur noch die übergabe an die präsentation:
