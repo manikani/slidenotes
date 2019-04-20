46d6ab3b80eb285deffbfef1ffb97dc4d801b882
jonny <dekan@gaenseklein.net>
2019-04-20 22:19:22 +0200

    fix path-issues for compatibility with webserver

--- themes/blocks.js
+++ themes/blocks.js
@@ -382,7 +382,7 @@ newtheme.styleThemeSpecials = function(){
       slidenote.presentation.getThemeByName("blocks").styleGrid();
       this.parentNode.removeChild(this);
     }
-    tmpimage.src = slidenote.basepath+"../images/lapa.jpg";
+    tmpimage.src = slidenote.imagespath+"lapa.jpg";
     document.getElementsByClassName("presentation")[0].appendChild(tmpimage);
   }
 }
